fa35d325477cd4dfd31c152c20fe3f99397e408b
Gordon Downs <gordiedowns@gmail.com>
2022-03-10 16:51:36 -0800

    print content with scores

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -286,7 +286,9 @@ class ModBot(discord.Client):
             return
         elif message.channel.name == f'group-{self.group_num}':
             scores = self.eval_text(message)
+            print(message.content)
             print(scores)
+            print()
             report_to_moderator = await self.eval_perspective_score(message, scores)
             await self.check_message_against_blacklist(message)
             if report_to_moderator:
